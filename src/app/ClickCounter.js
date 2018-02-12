a8c3089f6b7427c4152e790e48c1a2f5d9c51e72
ShawnCui <xiang.cui@pactera.com>
2018-02-12 08:55:08 +0800

    1.ClickCounter fix

--- src/app/ClickCounter.js
+++ src/app/ClickCounter.js
@@ -1,27 +1,23 @@
 import React, {Component} from 'react';
-import logo from '../logo.svg';
-import '../App.css';
 
 class ClickCounter extends Component {
     constructor(props) {
         super(props);
-        this.onClickButton().bind(this);
-        this.state1 = {count: 0};
+        this.state = {count: 0};
+        this.onClickButton = this.onClickButton.bind(this);
+    }
 
+    componentDidUpdate() {
     }
 
     onClickButton() {
-        this.setState1({count: this.state.count + 1})
+        this.setState({count: this.state.count + 1})
     }
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
+            <div>
+                <p>
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
                 <div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import ClickCounter from './app/ClickCounter';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<ClickCounter />, document.getElementById('root'));
-registerServiceWorker();
